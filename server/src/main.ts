37a82684b3f1fb6fdfc417ef1f4af9355bb17afa
Matvey V <main.monyster@gmail.com>
2023-06-04 11:16:36 +0300

    https

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./src/secrets/key.pem'),
+    cert: fs.readFileSync('./src/secrets/cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.enableCors();
+
   const configService: ConfigService = app.get(ConfigService);
 
   const port = configService.get<number>('port');
